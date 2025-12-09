7233df969ca215d1a359aa00bb6c6c789424fcc6
emmaprofemx <emmaprofemx@gmail.com>
2025-12-09 01:15:29 -0600

    Creacion de pantalla login

--- lib/src/presentation/pages/auth/login/LoginPage.dart
+++ lib/src/presentation/pages/auth/login/LoginPage.dart
@@ -6,8 +6,53 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('LoginPage'),
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.person,
+              color: Colors.black,
+              size: 125,
+            ) ,
+            Text(
+              'LOGIN' ,
+              style: TextStyle(
+                fontSize: 20 , 
+                fontWeight: FontWeight.bold
+              ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  label: Text('Correo electronico')
+                ),
+              ), TextField(
+                decoration: InputDecoration(
+                  label: Text('Password')
+                ),
+              ),
+              ElevatedButton(onPressed: (){}
+              , child: Text('INICIAR SESION' , style: TextStyle(
+                color: Colors.black
+              ),),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green
+              ),),
+              Text('No tiene cuenta?'),ElevatedButton(onPressed: (){}
+              , child: Text('INICIAR SESION' , style: TextStyle(
+                color: Colors.white
+              ),),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black
+              ),)
+
+
+
+
+          ],
+        ),
       ),
     );
   }
