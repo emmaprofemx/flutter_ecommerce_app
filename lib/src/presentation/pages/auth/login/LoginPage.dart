59a40bc0fb9c74f2c1149e6f2a95db900d315fc5
emmaprofemx <emmaprofemx@gmail.com>
2025-12-28 17:08:26 -0600

    Estableciendo el fondo de Login

--- lib/src/presentation/pages/auth/login/LoginPage.dart
+++ lib/src/presentation/pages/auth/login/LoginPage.dart
@@ -8,49 +8,53 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       body: Container(
         width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+        child: Stack(
           children: [
-            Icon(
-              Icons.person,
-              color: Colors.black,
-              size: 125,
-            ) ,
-            Text(
-              'LOGIN' ,
-              style: TextStyle(
-                fontSize: 20 , 
-                fontWeight: FontWeight.bold
-              ),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  label: Text('Correo electronico')
-                ),
-              ), TextField(
-                decoration: InputDecoration(
-                  label: Text('Password')
-                ),
-              ),
-              ElevatedButton(onPressed: (){}
-              , child: Text('INICIAR SESION' , style: TextStyle(
-                color: Colors.black
-              ),),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.green
-              ),),
-              Text('No tiene cuenta?'),ElevatedButton(onPressed: (){}
-              , child: Text('INICIAR SESION' , style: TextStyle(
-                color: Colors.white
-              ),),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.black
-              ),)
-
-
-
-
+            Image.asset('assets/img/background1.jpg',
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            fit: BoxFit.cover,),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.person,
+                  color: Colors.black,
+                  size: 125,
+                ) ,
+                Text(
+                  'LOGIN' ,
+                  style: TextStyle(
+                    fontSize: 20 , 
+                    fontWeight: FontWeight.bold
+                  ),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      label: Text('Correo electronico')
+                    ),
+                  ), TextField(
+                    decoration: InputDecoration(
+                      label: Text('Password')
+                    ),
+                  ),
+                  ElevatedButton(onPressed: (){}
+                  , child: Text('INICIAR SESION' , style: TextStyle(
+                    color: Colors.black
+                  ),),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green
+                  ),),
+                  Text('No tiene cuenta?'),ElevatedButton(onPressed: (){}
+                  , child: Text('INICIAR SESION' , style: TextStyle(
+                    color: Colors.white
+                  ),),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black
+                  ),)
+              ],
+            ),
           ],
         ),
       ),
